cde61f2ef6b6cd7d777fc9b9271a092b0a808cb2
Joseph Medina <josephmedina@Josephs-MacBook-Air.local>
2018-02-13 18:09:38 -0800

    Initial Commit.

---
+++ Project1.js
@@ -0,0 +1,54 @@
+const baseURL = "https://api.genderize.io/?name="; // The base URL
+
+// Gets the text field and sets up some variables for later
+let name;
+let gender;
+let probability;
+
+// Using the httpGetAsync function, we make a call to the API
+function getNameGender() {
+	name = document.getElementById("BoyOrGirl").value; // Sets the value of name to what is currently in the text field
+	let fullURL = baseURL + name; // Appends the name to the base URL
+	httpGetAsync(fullURL, assignGender); // Returns the API's response to the function assignGender
+}
+
+// Makes an async call for JSON (Vanilla Javascript)
+function httpGetAsync(theUrl, callback) {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)  
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+// Analyzes the data of the JSON response, then sets the webpage text
+function assignGender(data) {
+	let dataJSON = JSON.parse(data);
+	console.log(dataJSON);
+	gender = dataJSON.gender;
+	probability = dataJSON.probability;
+
+	if(probability == 1) {
+		probability = 100;
+	} else {
+		probability = probability * 100;
+	}
+
+	setText(name, gender, probability);
+}
+
+function setText(name, gender, probability) {
+	let nameString = "Right! So your name is " + name + "!";
+	let genderString = "I believe that you are a " + gender + ", correct?";
+	let probabilityString = "I'm about " + probability + "% certain.";
+
+	// Makes the element visible
+	document.getElementById("OakResponse").style.padding = "65pt";
+	document.getElementById("OakResponse").style.visibility = "visible";
+
+	document.getElementById("name").innerHTML = nameString;
+	document.getElementById("gender").innerHTML = genderString;
+	document.getElementById("probability").innerHTML = probabilityString;
+}
